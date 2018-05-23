d3769515586a2bbdb833daac5e265d6c7732af2c
rice_gavin@hotmail.com <rice_gavin@hotmail.com>
2018-05-23 19:25:09 +0100

    Adding Lint file watch

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('£0,00.00');
 
-console.log(`I would pay ${courseValue} for this course`);
+console.log(`I would pay ${courseValue} for this course`); //eslint-disable-line no-console
